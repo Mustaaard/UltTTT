af8969001c92c4b5d25331ca1f92d35b406256dc
Troglodyte112 <troykoran@gmail.com>
2022-05-11 13:58:25 -0700

    Fixed hitbox

--- src/ultttt/Piece.java
+++ src/ultttt/Piece.java
@@ -23,7 +23,7 @@ public class Piece {
     public void draw(Graphics2D g,int row,int column,int xdelta,int ydelta,String XorO) {
         g.setFont (new Font ("Arial",Font.PLAIN, 92));
         g.setColor(color);
-        g.drawString(XorO,Window.getX(column*xdelta), Window.getY(row*ydelta));
+        g.drawString(XorO,Window.getX(column*xdelta), Window.getY((row+1)*ydelta));
         g.setColor(Color.black);
     }
     
