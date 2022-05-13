e62b2f24dac46808960a1fe2a4951665ac51e56a
Mustaaard <fefebigboy@gmail.com>
2022-05-12 20:49:26 -0700

    Revert "Working Highlighter"
    This reverts commit deb7b5340797ef99719837e0b39579cff033d1e8.

--- src/ultttt/HighLight.java
+++ src/ultttt/HighLight.java
@@ -1,31 +1,24 @@
 package ultttt;
-import java.awt.Color;
 import java.awt.Graphics2D;
 
 public class HighLight {
-    private static int ydelta = Window.getHeight2()/3;
-    private static int xdelta = Window.getWidth2()/3;
+    private static int ydelta = Window.getHeight2()/9;
+    private static int xdelta = Window.getWidth2()/9;
     
-    private static int sectionX;
-    private static int sectionY;
-    
-    public static void getSection(int MouseXPos,int MouseYPos) {
+    public static int getSection(int MousePosX,int MousePosY) {
         int[][] sections = {{0,1,2},
                             {3,4,5},
                             {6,7,8}};
         int section;
         
-        int x = MouseXPos/(Window.getWidth2()/3);
-        int y = MouseYPos/(Window.getHeight2()/3);
-        
+        int x = MousePosX/Window.getWidth2()/3;
+        int y = MousePosX/Window.getHeight2()/3;
         section = sections[y][x];
         
-        sectionX = sections[y][x]-(sections[y][x]/3)*3;
-        sectionY = sections[y][x]/3;
+        return section;
     }
     
-    public static void draw(Graphics2D g) {
-        g.setColor(Color.yellow);
-        g.fillRect(Window.getX(0+xdelta*sectionX),Window.getY(0+ydelta*sectionY),xdelta,ydelta);
+    public static void draw(Graphics2D g,int MousePosX,int MousePosY) {
+        
     }
 }
