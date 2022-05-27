deabb4d17591a2043a657f88f93ae078b23c05f2
LtWalrus <lukearsenault410@gmail.com>
2022-05-27 14:40:29 -0700

    jiggy

--- src/ultttt/UltTTT.java
+++ src/ultttt/UltTTT.java
@@ -124,6 +124,10 @@ public class UltTTT extends JFrame implements Runnable {
                     bfgSound.stopPlaying=true;
                     bfgSound = new sound("RickRoll.wav");
                     }
+                    else if (e.VK_S == e.getKeyCode()) {
+                    bfgSound.stopPlaying=true;
+                    bfgSound = new sound("cnbt.wav");
+                    }
                     else if (e.VK_SPACE == e.getKeyCode()) {
                     if (!start) {
                         start = true;
